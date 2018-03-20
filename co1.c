4a94d457f5137d4b0efc9e7614cca8474df56bd8
nithyaganeshan <36395607+nithyaganeshan@users.noreply.github.com>
2018-03-20 16:05:38 +0530

    co1.c

---
+++ co1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char d;
+printf("enter the character");
+scanf("%c",&d);
+if(d=='a'||d=='e'||d=='i'||d=='o'||d=='u')
+{
+printf("vowel");
+}
+else
+{
+printf("not vowel");
+}
+}
